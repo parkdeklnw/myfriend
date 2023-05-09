fe55684d3f74616b3ee173d36bf7063c7b17dc0c
parkdeklnw <piyaphat14739@gmail.com>
2023-05-09 15:59:07 +0700

    profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('profile.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required|max:255|unique:profiles',
+                'tel' => 'required|max:255',
+                'email' => 'required|max:255|email|unique:profiles',
+                'detail' => 'required',
+            ]
+        );
+
+        $profile = new Profile();
+        $profile->name = $request->name;
+        $profile->tel = $request->tel;
+        $profile->email = $request->email;
+        $profile->detail = $request->detail;
+        $profile->save();
+
+        return redirect()->route('profile.index');
+    }
 }
